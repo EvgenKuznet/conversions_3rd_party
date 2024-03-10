abad245e28b74c656b6153a16391a2ed19074106
Evgeniy <evgenykuznet@gmail.com>
2024-03-10 11:11:16 +0300

    save submission as pd.Series, rewrite compute_score.py

--- compute_score.py
+++ compute_score.py
@@ -1,18 +1,20 @@
 from sklearn.metrics import roc_auc_score
 import pandas as pd
 
-
-def compute_metric(labels, pred):
-    score = roc_auc_score(labels, pred)
-    return score
-
-
-LABELS_PATH = "./data/private_info/private.csv"  # либо "./data/private_info/public.csv"
+PUBLIC_LABELS_PATH = "./data/private_info/public.csv"
+PRIVATE_LABELS_PATH = "./data/private_info/private.csv"
 SUBM_PATH = "./data/submission.csv"
+N_PUBLIC = 324726  # number or rows in public dataset
+
+def compute_metric(submission, public_labels, private_labels):
+    public_score = roc_auc_score(public_labels.values, submission[:N_PUBLIC])
+    private_score = roc_auc_score(private_labels.values, submission[N_PUBLIC:])
+    return public_score, private_score
 
 if __name__ == "__main__":
-    subm_df = pd.read_csv(SUBM_PATH, sep="\t")
-    labels_df = pd.read_csv(LABELS_PATH, sep="\t")
+    submission = pd.read_csv(SUBM_PATH, sep="\t")
+    public_labels = pd.read_csv(PUBLIC_LABELS_PATH, sep="\t")
+    private_labels = pd.read_csv(PRIVATE_LABELS_PATH, sep="\t")
 
-    metric = compute_metric(labels_df, subm_df)
-    print(f"ROC AUC: {metric}")
+    metric = compute_metric(submission, public_labels, private_labels)
+    print(f"ROC AUC public: {metric[0]}")
